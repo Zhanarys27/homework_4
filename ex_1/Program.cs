4ea81c064e5f7a699ea5d36acb01038005f78ca0
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-06-02 23:58:37 +0600

    add ex_1

---
+++ ex_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Clear();
+
+int StepenChisla(int num, int stepen)
+{
+    int count = 1;
+    for (int i = 0; i < stepen; i++)
+    {
+        count*=num;
+    }
+    return count;
+}
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите степень: ");
+int stepen = Convert.ToInt32(Console.ReadLine());
+
+int result = StepenChisla(num,stepen);
+System.Console.WriteLine($"Результат: {result}");
